d3ae966f535a2b695b9bd56014c1fd805e357362
shijiayao <shijiayao2014@xiaochuankeji.cn>
2022-06-28 18:01:40 +0800

    update test code

--- app/src/main/java/com/jiaoay/expandabletextview/MainActivity.kt
+++ app/src/main/java/com/jiaoay/expandabletextview/MainActivity.kt
@@ -26,37 +26,37 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
 
-//		listOf(binding.text3, binding.text5).forEachIndexed { index, expandableTextView ->
-//			expandableTextView.setContent(
-//				content = SpannableString(getString(textList[index])),
-//				expandState = false
-//			)
-//		}
-//
-//		binding.text1.setContent(
-//			SpannableString(getString(R.string.long_test_text)),
-//			false
-//		)
-//
-//		binding.text4.setContent(
-//			SpannableString(getString(R.string.hear_me)),
-//			expandState = false
-//		)
+		listOf(binding.text3, binding.text5).forEachIndexed { index, expandableTextView ->
+			expandableTextView.setContent(
+				content = SpannableString(getString(textList[index])),
+				expandState = false
+			)
+		}
+
+		binding.text1.setContent(
+			SpannableString(getString(R.string.long_test_text)),
+			false
+		)
+
+		binding.text4.setContent(
+			SpannableString(getString(R.string.hear_me)),
+			expandState = false
+		)
 
 		val appendText = getString(R.string.num_test_text)
 		val contentText = getString(R.string.short_test_text)
 
 		val spannableStringBuilder = SpannableStringBuilder(contentText)
 			.append(appendText)
-		ResourcesCompat.getDrawable(resources, R.drawable.ic_launcher_background, theme)?.let {
-			it.setBounds(0, 0, 16.dp2px, 16.dp2px)
-			spannableStringBuilder.setSpan(
-				ImageSpan(it),
-				contentText.length,
-				contentText.length + appendText.length,
-				Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-			)
-		}
+//		ResourcesCompat.getDrawable(resources, R.drawable.ic_launcher_background, theme)?.let {
+//			it.setBounds(0, 0, 16.dp2px, 16.dp2px)
+//			spannableStringBuilder.setSpan(
+//				ImageSpan(it),
+//				contentText.length,
+//				contentText.length + appendText.length,
+//				Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+//			)
+//		}
 
 		binding.text2.setContent(
 			content = spannableStringBuilder,
